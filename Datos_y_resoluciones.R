f8ee22658435f5842a4c45790afefd35ac32beb0
MatteoConte00 <matteo2.conte@mail.polimi.it>
2024-12-05 10:42:54 +0100

    Modifica codigo

--- Datos_y_resoluciones.R
+++ Datos_y_resoluciones.R
@@ -21,6 +21,12 @@ source("TDecisionCodigo/teoriadecision_funciones_multicriterio_utiles.R")
 tab<-multicriterio.crea.matrizdecision(c(850,0.95,7,180,820,0.92,10,200,880,0.98,5,160,900,0.9,8,150),
                                        4,4,c("A","B","C","D"),c("Costo","Affidabilità","Tempi di consegna","Impatto ambientale"))
 tab
+
+#El costo es un criterio de minimizar--> lo transformo para hacerlo un criterio
+#de maximizar
+tab[,1]<-1/tab[,1]
+tab
+
 tab2<-multicriterio.homogeneizacion.nadir(tab)
 tab2
 #-----Metodo AHP-----
@@ -29,17 +35,47 @@ tab2
 #* Costo<-2 por Affidabilità; Costo<-3 por Tempi di consegna; Costo<-4 por Impatto Ambientale
 #* Affidabilità<-1.5 por Tempi di consegna; Affidabilità<- 2 por Impatto Ambientale
 #* Tempi di consegna<- 4/3 por Impatto Ambientale
+install.packages("devtools")
+devtools::install_github("calote/ahp", build_vignettes = TRUE)
 tab3<-multicriterio.crea.matrizvaloraciones_mej(c(2,3,4,1.5,2,4/3),4,c("C1","C2","C3","C4"))
 tab3
-inc<-multicriterio.metodoAHP.coef.inconsistencia(tab3)
+tab3_1<-multicriterio.crea.matrizvaloraciones_mej(c(tab2[1,1]/tab2[2,1],tab2[1,1]/tab2[3,1],
+                                                    tab2[1,1]/(tab2[4,1]+0.01),tab2[2,1]/tab2[3,1],
+                                                    tab2[2,1]/(tab2[4,1]+0.01),tab2[3,1]/(tab2[4,1]+0.01))
+                                                  ,4,c("A","B","C","D"))
+tab3_1
+tab3_2<-multicriterio.crea.matrizvaloraciones_mej(c(tab2[1,2]/tab2[2,2],tab2[1,2]/tab2[2,3],
+                                                    tab2[1,2]/(tab2[4,2]+0.01),tab2[2,2]/tab2[3,2],
+                                                    tab2[2,2]/(tab2[4,2]+0.01),tab2[3,2]/(tab2[4,2]+0.01))
+                                                  ,4,c("A","B","C","D"))
+tab3_2
+tab3_3<-multicriterio.crea.matrizvaloraciones_mej(c(tab2[1,3]/tab2[2,3],tab2[1,3]/(tab2[3,3]+0.01),
+                                                    tab2[1,3]/tab2[4,3],tab2[2,3]/(tab2[3,3]+0.01),
+                                                    tab2[2,3]/tab2[4,3],(tab2[3,3]+0.01)/tab2[4,3])
+                                                  ,4,c("A","B","C","D"))
+tab3_3
+tab3_4<-multicriterio.crea.matrizvaloraciones_mej(c(tab2[1,4]/tab2[2,4],tab2[1,4]/tab2[3,4],
+                                                    tab2[1,4]/(tab2[4,4]+0.01),tab2[2,4]/tab2[3,4],
+                                                    tab2[2,2]/(tab2[4,4]+0.01),tab2[3,4]/(tab2[4,4]+0.01))
+                                                  ,4,c("A","B","C","D"))
+tab3_4
 
-pesos<-multicriterio.metodoAHP.variante3.basico(tab3)
-pesos_criterios<-pesos$valoraciones.ahp
-rank<-tab2%*%pesos_criterios
-rank
-#El metodo AHP nos sugiere el provedoor C
+aut<-multicriterio.metodoAHP.variante1.autovectormayorautovalor(tab3)
+aut1<-multicriterio.metodoAHP.variante1.autovectormayorautovalor(tab3_1)
+aut2<-multicriterio.metodoAHP.variante1.autovectormayorautovalor(tab3_2)
+aut3<-multicriterio.metodoAHP.variante1.autovectormayorautovalor(tab3_3)
+aut4<-multicriterio.metodoAHP.variante1.autovectormayorautovalor(tab3_4)
 
-##Metodo ELECTREE
+pesos_glob<-multicriterio.metodoAHP.pesosglobales_entabla(aut$valoraciones.ahp,
+                                                     rbind(aut1$valoraciones.ahp,
+                                                           aut2$valoraciones.ahp,
+                                                           aut3$valoraciones.ahp,
+                                                           aut4$valoraciones.ahp))
+pesos_glob
+
+#El metodo AHP nos sugiere el provedoor B, porquè tiene el mayor peso global
+
+#----Metodo ELECTRE----
 
 #Como pesos utilizo los siguientes:
 #* Costo<-0.4
@@ -51,30 +87,33 @@ install.packages("qgraph")
 library(qgraph)
 pesos<-c(0.4,0.3,0.2,0.1)
 alpha<-0.7
-no_se_compensan<-c(20,0.03,2,10)
-el1<-multicriterio.metodoELECTRE_I(tab2,pesos,alpha,no_se_compensan)
+no_se_compensan<-c(0.0001,0.03,2,15)
+el1<-multicriterio.metodoELECTRE_I(tab,pesos,alpha,no_se_compensan)
+el1
 el<-func_ELECTRE_Completo(el1)
 qgraph(el$Grafo)
+el2<-multicriterio.metodoELECTRE_I(tab,pesos,alpha,no_se_compensan,que.alternativas = c(2,3))
+el2
 alpha<-0.55
-el2<-multicriterio.metodoELECTRE_I(tab2,pesos,alpha,no_se_compensan)
-el<-func_ELECTRE_Completo(el2)
-qgraph(el$Grafo)
+el3<-multicriterio.metodoELECTRE_I(tab,pesos,alpha,no_se_compensan,que.alternativas = c(2,3))
+el3
 
-#El metodo Electree I nos sugiere el provedoor C, como con el primer metodo
+#El metodo Electre no nos da un provedoor mejor, pero al menos elimina dos alternativas (A y D)
+#Tambien en este caso tenemos B como alternativa valida (asì como C claramente)
 
-##Metodo Promethee
+#----Metodo Promethee-----
 
 pesos<-c(0.4,0.3,0.2,0.1)
-tab_pref<-matrix(c(3,10,15,0,
+tab_pref<-matrix(c(5,0.001,0.002,0,
                    5,0.02,0.05,0,
                    3,1,3,0,
-                   3,5,10,0),ncol=4,byrow=T)
-promethee_1<-multicriterio.metodo.promethee_i(tab2,pesos,tab_pref)
+                   3,8,17,0),ncol=4,byrow=T)
+promethee_1<-multicriterio.metodo.promethee_i(tab,pesos,tab_pref)
 library(qgraph)
 qgraph::qgraph(promethee_1$tablarelacionsupera)
 
-multicriterio.metodo.promethee_ii(tab2,pesos,tab_pref)
+multicriterio.metodo.promethee_ii(tab,pesos,tab_pref)
 
-#Ambos metodos Promethee (I y II) nos sugieren que el provedoor mejor es el C
-#En conclusion, se puede decir que el provedoor C es lo que elegimos,
+#Ambos metodos Promethee (I y II) nos sugieren que el provedoor mejor es el B
+#En conclusion, se puede decir que el provedoor B es lo que elegimos,
 #porquè resulta el mejor en todos los metodos multicriterios utilizados
